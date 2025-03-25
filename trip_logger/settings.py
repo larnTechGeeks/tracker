9a17b18518917d0f6aca6d15349830742c5c33c5
larntechGeeks <larntechconsultants@gmail.com>
2025-03-25 15:31:49 +0300

    deploy configurations

--- trip_logger/settings.py
+++ trip_logger/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$)l@c6!ob)yukha#yxt8nc9ca))ha5u@xzh=+mi=shrxow-r^!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".vercel.app"]
 
 # Application definition
 
